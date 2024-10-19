816889394f4b9e1fd5221a2671ee2606ebfc6f74
zainab <zainabab789@gmail.com>
2024-10-19 18:28:04 +0500

    first.commit

---
+++ index.js
@@ -0,0 +1,33 @@
+import inquirer from 'inquirer';
+const currency = {
+    USD: 1, //BASE CURRENCY
+    EUR: 0.91,
+    GBP: 0.76,
+    IMR: 74.57,
+    PKR: 280
+};
+let user_answer = await inquirer.prompt([
+    {
+        name: 'from',
+        message: "enter from currency",
+        type: 'list',
+        choices: ['USD', 'EUR', 'GBP', 'IMR', 'PKR']
+    },
+    {
+        name: 'to',
+        message: "enter to currency",
+        type: 'list',
+        choices: ['USD', 'EUR', 'GBP', 'IMR', 'PKR']
+    },
+    {
+        name: 'amount',
+        messag: "enter your Amount",
+        type: 'number'
+    }
+]);
+let fromamount = currency[user_answer.from];
+let totalAmount = currency[user_answer.to];
+let amount = user_answer.amount;
+let baseamount = amount / fromamount; //USD base currency // 4
+let convertedAmount = baseamount * totalAmount;
+console.log(convertedAmount);
